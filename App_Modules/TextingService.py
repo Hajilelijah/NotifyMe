23fe4e2867e564d407b6023528023d4481c81c4a
Elijah Phifer <elijah.phifer@selu.edu>
2023-07-24 13:22:50 -0500

    the beginnnings

--- App_Modules/TextingService.py
+++ App_Modules/TextingService.py
@@ -1,1 +1,30 @@
 import smtplib
+import ssl
+from email.message import EmailMessage
+import json
+
+with open("App_Configs\\TextingService.config.json", 'r') as file:
+    config = json.load(file)
+
+# Define email sender and receiver
+email_sender = 'write-email-here'
+email_password = 'write-password-here'
+email_receiver = 'write-email-receiver-here'
+
+# Set the subject and body of the email
+subject = 'Check out my new video!'
+body = """ """
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+# Add SSL (layer of security)
+context = ssl.create_default_context()
+
+# Log in and send the email
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
